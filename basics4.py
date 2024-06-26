a123a40dc0dbc88070a82be5e1d16a764495e0fe
Kaleb Franken <kaleb.franken7@gmail.com>
2024-06-26 16:00:36 -0700

    basics 4

---
+++ basics4.py
@@ -0,0 +1,10 @@
+marks = int( input ("Enter marks") )
+
+if marks >= 90:
+    print ("grade A")
+elif marks >= 70:
+    print ('grade b')
+elif marks >= 60:
+    print ('grade c')
+elif marks >= 50:
+    print ('grade d')
